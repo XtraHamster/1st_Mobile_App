856ff74d27195ecc0c0bf1a8a8cba7e6fbacb6bf
XtraHamster <martinkrecek98@seznam.cz>
2020-07-18 11:58:05 +0200

    Login page
    Added login page funcionality. User can now login with its username and password. If correct credentials are provided, user is redirected to app with option Log out button and will be redirected to a Login page. If wrong credentials are provided, Label with info about wrong input will pop up.

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ Builder.load_file('design.kv')
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = 'sign_up_screen'
+    def login(self, uname, pword):
+        with open('users.json') as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pword:
+            self.manager.current = 'login_screen_success'
+        else:
+            self.ids.login_wrong.text = 'Wrong username or password'
 
 class RootWidget(ScreenManager):
     pass
@@ -28,6 +35,11 @@ class SignUpScreen(Screen):
 
 class SignUpScreenSuccess(Screen):
     def login_screen(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login_screen'
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
         self.manager.transition.direction = 'right'
         self.manager.current = 'login_screen'
 
